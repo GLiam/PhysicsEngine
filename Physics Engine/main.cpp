1af2cfb46b0d81d1c83973f5646e224a44cd6076
AIE\s162871 <liamcantfly@gmail.com>
2018-03-14 16:41:24 +1100

    class Diagram started

--- Physics Engine/main.cpp
+++ Physics Engine/main.cpp
@@ -5,6 +5,8 @@ int main() {
 	// allocation
 	auto app = new Physics_EngineApp();
 
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_ALLOC_MEM_DF);
+
 	// initialise and loop
 	app->run("AIE", 1280, 720, false);
 
